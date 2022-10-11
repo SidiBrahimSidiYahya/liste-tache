4b9871967cf3467c188f22409b7571ef360963fd
Sidi Brahim Sidi Yahya <sb.sidiyahya@gmail.com>
2022-10-11 09:47:17 +0000

    icluding modul exports to project

--- app.js
+++ app.js
@@ -1,26 +1,49 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date");
+
 const app = express();
+
 app.set('view engine', 'ejs');
+
 app.use(bodyParser.urlencoded({extended:true}));
-var items =["Buy food","cook food","eat food"];
+app.use(express.static(__dirname + '/public'));
+
+let items =["Buy food","cook food","eat food"];
+let workItems =[];
+
 app.get('/',function(req,res){
-    var today = new Date();
-    var option={
-        weekday:"long",
-        day:"numeric",
-        month:"long",
-    }
-    var day = today.toLocaleDateString("en-US", option)
     
-    res.render("index",{day:day,newListItem:items});
     
-})
+    res.render("index",{listTitle:date.getDay(),newListItem:items});
+    
+});
+
+app.get('/work',function(req,res){
+
+    res.render('index', {listTitle:"work List",newListItem:workItems})
+});
+
+
 app.post("/",function(req,res){
-    var item = req.body.newItem;
+    let item = req.body.newItem;
+    
+    if(req.body.list === "work List"){
+        workItems.push(item);
+        res.redirect('/work')
+    }else{
     items.push(item);
     res.redirect("/");
+    };
+});
+
+app.post('/work',function(req,res){
+    let item = req.body.newItem;
+    workItems.push(item);
+    res.redirect("/work")
 })
+
+
 app.listen(5000,function(){
     console.log("you have listerning in port 5000")
 })
---
+++ date.js
@@ -0,0 +1,9 @@
+exports.getDay = function(){
+    let today = new Date();
+    let option={
+        weekday:"long",
+        day:"numeric",
+        month:"long",
+    }
+    return today.toLocaleDateString("en-US", option);
+}
